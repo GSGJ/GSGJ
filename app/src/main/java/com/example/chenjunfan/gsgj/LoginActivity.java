3917cd085b2cefbcbf72c3811b8f8863d5c072d7
AfricanGigolo <625517361@qq.com>
2016-12-04 16:36:41 +0800

    增加主页面
    陈俊帆

--- app/src/main/java/com/example/chenjunfan/gsgj/LoginActivity.java
+++ app/src/main/java/com/example/chenjunfan/gsgj/LoginActivity.java
@@ -1,12 +1,31 @@
 package com.example.chenjunfan.gsgj;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button BTlogin,BTregist;
+    private EditText ETid,ETpw;
+
+
     public void Init()
     {
+        BTlogin = (Button) findViewById(R.id.bt_lg_login);
+        BTregist = (Button) findViewById(R.id.bt_lg_regist);
+        ETid = (EditText) findViewById(R.id.et_lg_id);
+        ETpw = (EditText) findViewById(R.id.et_lg_pw);
+    }
+
+    public void PressLogin(View view)
+    {
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
 
     }
 
@@ -14,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Init();
 
     }
 }
